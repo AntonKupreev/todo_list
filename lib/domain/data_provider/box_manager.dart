f67e36cafc631558b6824c5801b297b0defb52ba
Anton <madrid.87@mail.ru>
2022-05-25 18:56:49 +0300

    1

--- lib/domain/data_provider/box_manager.dart
+++ lib/domain/data_provider/box_manager.dart
@@ -1,3 +1,22 @@
+
+import 'package:hive/hive.dart';
+import 'package:todo_list/domain/entity/group.dart';
 class  BoxManager {
-  
+ Future<Box<Group>> openGroupBox() async{
+   return _openBox('group_box', 1, GroupAdapter());
+ }
+ Future<Box<Task>> openTaskBox() async{
+   return _openBox('tasks_box', 2, TaskAdapter());
+ }
+
+ Future<Box<T>>_openBox<T>(String name,
+  int typeID,
+  TypeAdapter<T>adapter,
+  ) async {
+   if (!Hive.isAdapterRegistered(1)) {
+      Hive.registerAdapter(adapter);
+    }
+
+    return Hive.openBox<T>('group_box');
+ }
 }
